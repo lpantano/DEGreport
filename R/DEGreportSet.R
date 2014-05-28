d658b1a2017da27121bec33cc1fd1d64bc761b8e
Marc Carlson <mcarlson@fhcrc.org>
2014-05-28 17:02:13 +0000

    Adds DEGreport to the repos.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DEGreport@90775 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/DEGreportSet.R
@@ -0,0 +1,9 @@
+#' @name DEGreportSet
+#' @title list object for DE genes betwen Male and Females
+#' @description list of objects containgin counts matrix,g1,g2 and edgeR glmfit object
+#' @docType data
+#' @usage DEGreportSet
+#' @format matrix,list,list and matrix
+#' @source gEUvadis
+#' @author Lorena Pantano, 2014-05-31
+NULL
