11827e96b645a711d93e66529770c5c801b2369a
Lorena Pantano <lorena.pantano@gmail.com>
2017-08-16 19:32:58 -0400

    new sticker [skip ci]

--- inst/sticker/sticker.R
+++ inst/sticker/sticker.R
@@ -1,8 +1,13 @@
+library(ggplot2)
+library(ggimage)
+
 p <- ggplot(data.frame(x=1,y=1,image="inst/sticker/flag_hex.png"), aes(x,y)) +
     geom_image(aes(image=image), size=1.06) + theme_void()
 
 hexSticker::sticker(p,
-                    package="DEGreport", 
+                    url = "www.bioconductor.org",
+                    u_size = 5,
+                    package = "DEGreport", 
                     s_x = 1.03,
                     s_y = 0.98, 
                     s_width = 2.05,
