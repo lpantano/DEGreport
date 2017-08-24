093ff0c39a116e99ae903757b1bed370a03db3d1
Lorena Pantano <lorena.pantano@gmail.com>
2017-08-24 18:10:01 -0400

    Change condition in testthat class

--- tests/testthat/test_class.R
+++ tests/testthat/test_class.R
@@ -8,8 +8,8 @@ test_that("DEGSet",{
     dds <- DESeq(dds)
     res <- degComps(dds, combs = c("condition"))
 
-    expect_false("lfcSE" %in% (degTable(res[[1]], "shrunk") %>% names))
-    expect_true("lfcSE" %in% (degTable(res[[1]], "raw") %>% names))
+    expect_true(abs(degTable(res[[1]], "raw")[["log2FoldChange"]][[1]]) >
+                    abs(degTable(res[[1]], "shrunk")[["log2FoldChange"]][[1]]))
     expect_match(degDefault(res[[1]]), "shrunk")
     expect_error(degTable(res[[1]], "fake"))
     expect_identical(res[[1]][["shrunk"]], degTable(res[[1]]))
