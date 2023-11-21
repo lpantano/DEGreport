1b07612410bde41ecf1af6f7053ec3a9afdfd999
Lorena Pantano <lorena.pantano@gmail.com>
2023-11-21 15:23:26 -0500

    comment more test code to check non-reproducible error

--- tests/testthat/test_cluster.R
+++ tests/testthat/test_cluster.R
@@ -24,23 +24,23 @@ test_that("cluster_plot", {
 # 
 # })
 
-test_that("transform", {
-    expect_gte(mean(.scale(counts(dse)[1,])), -0.5)
-    expect_lte(mean(.scale(counts(dse)[1,])), 0.5)
-    countsGroup <- .summarize_scale(counts(dse)[1:100,],
-                                    colData(dse)[["group"]])
-    expect_equal(mean(countsGroup), 0)
-    hc <- .make_clusters(countsGroup)
-    cluster0 <- .select_genes(hc, countsGroup, minc = 5, reduce = TRUE)
-    cluster <- .select_genes(hc, countsGroup, minc = 5)
-    expect_equal(unique(cluster), c(1, 2))
-    expect_equal(unique(cluster0), c(1, 2))
-    df <- data.frame(cluster = cluster, genes = names(cluster))
-    expect_equal(.median_per_cluster(countsGroup, df) %>% mean, 0)
-    expect_equal(.filter(df, 50) %>% nrow, 69)
-    expect_equal(.group_metadata(as.data.frame(colData(dse)),
-                                 "group", "group", "group") %>% nrow, 2)
-})
+# test_that("transform", {
+#     expect_gte(mean(.scale(counts(dse)[1,])), -0.5)
+#     expect_lte(mean(.scale(counts(dse)[1,])), 0.5)
+#     countsGroup <- .summarize_scale(counts(dse)[1:100,],
+#                                     colData(dse)[["group"]])
+#     expect_equal(mean(countsGroup), 0)
+#     hc <- .make_clusters(countsGroup)
+#     cluster0 <- .select_genes(hc, countsGroup, minc = 5, reduce = TRUE)
+#     cluster <- .select_genes(hc, countsGroup, minc = 5)
+#     expect_equal(unique(cluster), c(1, 2))
+#     expect_equal(unique(cluster0), c(1, 2))
+#     df <- data.frame(cluster = cluster, genes = names(cluster))
+#     expect_equal(.median_per_cluster(countsGroup, df) %>% mean, 0)
+#     expect_equal(.filter(df, 50) %>% nrow, 69)
+#     expect_equal(.group_metadata(as.data.frame(colData(dse)),
+#                                  "group", "group", "group") %>% nrow, 2)
+# })
 
 test_that("groupDifference", {
     ma <- matrix(rnorm(50), ncol = 2)
